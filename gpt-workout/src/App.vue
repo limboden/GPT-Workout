8c2e13bb80328cd7d5537b04028c19587993c93c
limboden <99365731+limboden@users.noreply.github.com>
2024-01-01 18:23:39 -0500

    THIS is the updated app.vue
    here it is

--- gpt-workout/src/App.vue
+++ gpt-workout/src/App.vue
@@ -1,47 +1,73 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello World!',
+      isMale: false,
+      isLBS: true,
+      weight: 0,
+      height: 0,
+      daysAWeek: 0,
+      feet: null,
+      inches: null,
+    } 
+  }, 
+
+  methods: {
+    toggleGender() {
+      this.isMale = !this.isMale
+    },
+    toggleWeightMeasurement() {
+      if(this.isLBS){
+        this.weight = this.weight / 2.2046
+      } else {
+        this.weight = this.weight * 2.2046
+      }
+      this.isLBS = !this.isLBS
+    },
+    output() {
+      //call upon the gpt api to get an output from our saved data
+    },
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h1>{{ isMale ? 'MALE' : 'FEMALE' }}</h1>
+  <button @click="toggleGender">Switch to {{ isMale ? 'FEMALE' : 'MALE' }}</button>
+  <h1>Weight: {{ weight }} {{isLBS ? 'LBS' : 'KG'}}</h1>
+  
+  <input type="number" v-model="weight"> {{isLBS ? 'LBS' : 'KG'}}
+  <button @click="toggleWeightMeasurement">Switch to {{ isLBS ? 'KG' : 'LBS' }}</button>
+  <h1>Height: {{ feet }}, {{inches}}</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <select v-model="feet" name="feet" id="feet">
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+  </select>'
 
-  <main>
-    <TheWelcome />
-  </main>
+  <select v-model="inches" name="inches" id="inches">
+    <option value="0">0</option>
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    <option value="9">9</option>
+    <option value="10">10</option>
+    <option value="11">11</option>
+  </select>"
+
+  <h1>Days a week: {{daysAWeek}}</h1>
+  <input type="number" v-model="daysAWeek"> days
+
+
+
+
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
