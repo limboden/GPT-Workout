378f86d875ca50fa8112be3885a19ea4e7bc9b5e
Lukas Imboden <imbodenlu@gmail.com>
2024-01-02 06:58:27 -0500

    front end html complete?
    so you enter in your data and then it displays what would be sent to chatGPT.
    I still have some elements that are just for show, like how it displays things in html just to show you how things are changing in the background, those we're just for me i guess.

--- gpt-workout/src/App.vue
+++ gpt-workout/src/App.vue
@@ -2,7 +2,6 @@
 export default {
   data() {
     return {
-      msg: 'Hello World!',
       isMale: false,
       isLBS: true,
       weight: 0,
@@ -10,6 +9,9 @@ export default {
       daysAWeek: 0,
       feet: null,
       inches: null,
+      showUserInputToGPT: false,
+      userInputToGPT: "test",
+
     } 
   }, 
 
@@ -23,51 +25,24 @@ export default {
       } else {
         this.weight = this.weight * 2.2046
       }
-      this.isLBS = !this.isLBS
+      this.isLBS = !this.isLBS;
     },
     output() {
-      //call upon the gpt api to get an output from our saved data
+      //call upon the gpt api to get output from our saved data
+    },
+    submitData() {
+      const gender = this.isMale ? 'male' : 'female'
+      const weightMeasurement = this.isLBS ? 'lbs' : 'kg'
+      this.userInputToGPT = "Create a workout routine for me. I am a " 
+      + gender + ", I am "
+      + this.weight + " " + weightMeasurement +
+      " , I am " 
+      + this.feet + " foot " + 
+      this.inches + ", and I want to workout " 
+      + this.daysAWeek +  " days a week."
     },
   }
 }
 </script>
 
-<template>
-  <h1>{{ isMale ? 'MALE' : 'FEMALE' }}</h1>
-  <button @click="toggleGender">Switch to {{ isMale ? 'FEMALE' : 'MALE' }}</button>
-  <h1>Weight: {{ weight }} {{isLBS ? 'LBS' : 'KG'}}</h1>
-  
-  <input type="number" v-model="weight"> {{isLBS ? 'LBS' : 'KG'}}
-  <button @click="toggleWeightMeasurement">Switch to {{ isLBS ? 'KG' : 'LBS' }}</button>
-  <h1>Height: {{ feet }}, {{inches}}</h1>
-
-  <select v-model="feet" name="feet" id="feet">
-    <option value="4">4</option>
-    <option value="5">5</option>
-    <option value="6">6</option>
-    <option value="7">7</option>
-    <option value="8">8</option>
-  </select>'
-
-  <select v-model="inches" name="inches" id="inches">
-    <option value="0">0</option>
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-    <option value="6">6</option>
-    <option value="7">7</option>
-    <option value="8">8</option>
-    <option value="9">9</option>
-    <option value="10">10</option>
-    <option value="11">11</option>
-  </select>"
-
-  <h1>Days a week: {{daysAWeek}}</h1>
-  <input type="number" v-model="daysAWeek"> days
-
-
-
-
-</template>
+<template src="./template.html"></template>
