3f471240fbb6cf911604072dccf9a9f787f3140f
Lukas Imboden <imbodenlu@gmail.com>
2024-02-16 09:07:34 -0500

    Changed inputs
    Changed inputs so theres mins and maxes that make sense, like you shouldn't be negative pounds, or you cant workout 9 days a week.
    Also I commented out the api code that I was working on before, because there's still something wrong with it, will figure that out today. Also added the api key to the git ignore, that's important.

--- gpt-workout/src/main.js
+++ gpt-workout/src/main.js
@@ -1,4 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// import { Configuration, OpenAIApi } from "openai";
+
+
+// const configuration = new Configuration({
+//   organization: "org-I6DQ77co3qIhsdPR2OPdfB8U",
+//   apiKey: "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",
+// });
+
+// const openai = new OpenAIApi(configuration);
+
+// const completion = await openai.createChatCompletion({
+//   model: "gpt-3.5-turbo",
+//   messages: [
+//     { role: "user", content: "Hello World" },
+//   ]
+
+// })
+
+// console.log(completion.data.choices[0].message);
+
 createApp(App).mount('#app')
